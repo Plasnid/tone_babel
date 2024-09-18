83d35208a8bf1d0326bdffc98f44f8ec550e2a8c
Plasnid <stuart@sackler.ca>
2024-09-17 21:53:10 -0400

    adding examples for coding note length and timing and chords

--- src/index.js
+++ src/index.js
@@ -1,61 +1,40 @@
 //npm install tone
 import * as Tone from "tone";
 
+//* documentation for timing is here:  https://github.com/Tonejs/Tone.js/wiki/Time
+
 const synth = new Tone.Synth({
     oscillator:{
         type: "sine" // can be sine, square, triangle, or sawtooth
     }
 }).toDestination();
 
+const pSynth = new Tone.PolySynth({
+    oscillator:{
+        type: "sine"
+    }
+}).toDestination();
+
 //* What about writing a tune?
 window.addEventListener("click", ()=>{
     console.log("play de music!");
-    // use an array of objects as long as the object has a "time" attribute
-    let now = Tone.now();
-    let part = new Tone.Part(((time, value) => {
-        // the value is an object which contains both the note and the velocity
-        synth.triggerAttackRelease(value.note, "8n",time, value.velocity);
-    }), [{ time: "0:0:0", note: "C3", velocity: 0.9 },  //bars:quarterNotes::16 notes
-        { time: "0:1:0", note: "C3", velocity: 0.5 },
-        { time: "0:2:0", note: "C3", velocity: 0.5 },
-        { time: "0:3:0", note: "D3", velocity: 0.5 },
-        { time: "0:3:2", note: "E3", velocity: 0.5 },
+    //schedule a series of notes, one per second
+    /*
+    synth.triggerAttackRelease('C4', 0.5, 0)
+    synth.triggerAttackRelease('E4', 0.5, 1)
+    synth.triggerAttackRelease('G4', 0.5, 2)
+    synth.triggerAttackRelease('B4', 0.5, 3)
+    */
 
-        { time: "1:0:2", note: "E3", velocity: 0.5 },
-        { time: "1:1:2", note: "D3", velocity: 0.5 },
-        { time: "1:2:0", note: "E3", velocity: 0.5 },
-        { time: "1:3:0", note: "F3", velocity: 0.5 },
-        { time: "1:3:2", note: "G3", velocity: 0.5 },
-
-        { time: "2:0:0", note: "C4", velocity: 0.5 },
-        { time: "2:0:2", note: "C4", velocity: 0.5 },
-        { time: "2:0:3", note: "C4", velocity: 0.5 },
-
-        { time: "2:1:0", note: "G3", velocity: 0.5 },
-        { time: "2:1:2", note: "G3", velocity: 0.5 },
-        { time: "2:1:3", note: "G3", velocity: 0.5 },
-
-        { time: "2:2:0", note: "E3", velocity: 0.5 },
-        { time: "2:2:2", note: "E3", velocity: 0.5 },
-        { time: "2:2:3", note: "E3", velocity: 0.5 },
-
-        { time: "2:3:0", note: "C3", velocity: 0.5 },
-        { time: "2:3:2", note: "C3", velocity: 0.5 },
-        { time: "2:3:3", note: "C3", velocity: 0.5 },
-
-        { time: "3:0:0", note: "G3", velocity: 0.5 },
-        { time: "3:0:3", note: "F3", velocity: 0.5 },
-        { time: "3:1:0", note: "E3", velocity: 0.5 },
-        { time: "3:1:3", note: "D3", velocity: 0.5 },
-        { time: "3:2:0", note: "C3", velocity: 0.5 },
-    ]).start(now);
-    Tone.Transport.start(0);
+    //schedule a series of notes to play
+    //synth.triggerAttackRelease(note, duration, time when note starts playing)
+    synth.triggerAttackRelease('C4', '4n', '+0:0:0')
+    synth.triggerAttackRelease('E4', '8n', '+0:1:1')
+    synth.triggerAttackRelease('G4', '16n', '+0:2:0')
+    synth.triggerAttackRelease('B4', '16n', '+0:2:1')
+    synth.triggerAttackRelease('G4', '16', '+0:2:2')
+    synth.triggerAttackRelease('E4', '2n', '+0:3:0')
+    
+    //* and if you want to make chords. here is an example
+    pSynth.triggerAttackRelease(["C2", "E2", "G2", "B2"], "4n", '+0:3:3');
 })
-/**[{ time: 0, note: "D3", velocity: 0.9 },
-    { time: "0:1", note: "D3", velocity: 0.5 },
-    { time: "0:2", note: "E3", velocity: 0.5 },
-    { time: "0:4", note: "D3", velocity: 0.5 },
-    { time: "0:6", note: "G3", velocity: 0.5 },
-    { time: "0:8", note: "F#3", velocity: 0.5 },
-]
-**/
